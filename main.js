55b7996aa6805960ad573a28c18de5b43bb1b883
Lhyck <3ksablanke@gmail.com>
2024-05-26 17:55:01 -0300

    probando ...

--- main.js
+++ main.js
@@ -46,3 +46,24 @@ function redirectUrl() {
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", onClick);
 
+//json de fragancias/
+
+/// ¿El navegador soporta esta función?
+if (typeof(Storage) !== "undefined") {
+// setItem guarda datos en el dispositivo
+localStorage.setItem("apellido", "Perez")
+localStorage.setItem("nombre", "Juan")
+console.log("Datos guardados.")
+} else {
+console.log("Web Storage no soportado.")
+}
+// ¿El navegador soporta esta función?
+if (typeof(Storage) !== "undefined") {
+// getItem recupera datos del dispositivo
+ape = localStorage.getItem("apellido")
+nom = localStorage.getItem("nombre")
+console.log(ape + ", " + nom)
+} else {
+console.log("Web Storage no soportado.")
+}
+
